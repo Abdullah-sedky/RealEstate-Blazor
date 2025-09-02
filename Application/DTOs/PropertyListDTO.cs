65b9d59bdd483b7934cf72919594f40153a13146
Abdullah-sedky <abdullah.sedky8@gmail.com>
2025-09-02 22:38:57 +0300

    first DTO

---
+++ Application/DTOs/PropertyListDTO.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Application.DTOs
+{
+    public class PropertyListDTO
+    {
+        public int PropertyId {  get; set; }
+        public string Name { get; set; } = null!; 
+        public string Description { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public PropertyType PropertyType { get; set; }
+        public PropertyStatus Status { get; set; }
+        public string? PrimaryPhotoUrl { get; set; }
+
+
+    }
+}
---
+++ Application/Mappers/PropertyMapper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Entities;
+using Application.DTOs;
+namespace Application.Mappers
+{
+    public class PropertyMapper
+    {
+        public static PropertyListDTO ToListDTO(Property property)
+        {
+            return new PropertyListDTO
+            {
+                PropertyId = property.PropertyId,
+                Name = property.Name,
+                Status = property.Status,
+                Price = property.Price,
+                PropertyType = property.PropertyType,
+                Description = property.Description,
+                PrimaryPhotoUrl = property.Photos.FirstOrDefault(p => p.IsPrim)?.ImageUrl
+            };
+        }
+        public static List<PropertyListDTO> ToListDTOs(List<Property> properties)
+        {
+            return properties.Select(ToListDTO).ToList();
+        }
+    }
+}
