a4383782849ddd876438f9f29340e5a8af01913e
Abdullah-sedky <abdullah.sedky8@gmail.com>
2026-04-28 10:39:12 +0300

    Commit

--- Domain/Entities/Agent.cs
+++ Domain/Entities/Agent.cs
@@ -1,15 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Domain.Entities;
 
 namespace Domain.Entities
 {
-     public class Agent : ApplicationUser
+    public class Agent : ApplicationUser
     {
-        public int AgentId { get; set; }
         public int YOE { get; set; }
         public int TotalDeals { get; set; }
         public int ClosedDeals { get; set; }
@@ -23,11 +18,7 @@ namespace Domain.Entities
         public string FacebookProf { get; set; } = string.Empty;
         public string ProfilePicUrl { get; set; } = string.Empty;
 
-        public List<ManagedProperty>? ManagedProperties;
-        public List<Notification>? Notifications;
-        public List<Task>? Tasks;
-
-        public string UserId { get; set; } = null!; 
-        public ApplicationUser User { get; set; } = null!; // Navigation property
+        public List<ManagedProperty> ManagedProperties { get; set; } = new();
+        public List<Notification> Notifications { get; set; } = new();
     }
 }
